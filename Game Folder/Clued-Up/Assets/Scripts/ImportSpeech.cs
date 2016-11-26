709a53f548f7b6c0206a77984cbdd977ebc8eec8
DESKTOP\simon <smd517@york.ac.uk>
2016-11-26 18:16:56 +0000

    Added multiple branches per file, item and character references
    Once I get the definitions for items and characters, I'll change
    ItemText and OtherChar to actually pull the names directly. Also need to
    do documentation.

--- Game Folder/Clued-Up/Assets/Scripts/ImportSpeech.cs
+++ Game Folder/Clued-Up/Assets/Scripts/ImportSpeech.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using System.Collections.Generic;
 
 public class ImportSpeech : MonoBehaviour {
 
@@ -8,51 +9,85 @@ public class ImportSpeech : MonoBehaviour {
 	public Vector3 LeftPos;  // THIS CO-ORDINATE WANTS TO BE WHERE YOU WANT ( TO APPEAR
 	public Vector3 RightPos;   // SAME BUT FOR )
 	public Vector3 NeutralPos; // SAME BUT FOR WHEN YOU'RE DONE
-	private string AssetText;
+	public string ItemName;
+	public string OtherChar;
+	private int Pos = 0;
+	private ArrayList BranchList;
 	private ArrayList SpeechList;
-	private int Pos;
-	private int Index;
+	private Dictionary<string, ArrayList> SpeechDict;
 
 	// Use this for initialization
-	void Start () {
-		Pos = 0;
-		SpeechList = new ArrayList ();	// Make sure speeck starts as an empty list
-		AssetText = asset.text; 		// Read the file
+	private void Start () {
+		SpeechDict = new Dictionary<string, ArrayList>();
+		SpeechList = new ArrayList ();
+		string AssetText = asset.text; 		// Read the file
 		AssetText = AssetText.Replace("\r", "").Replace("\n", "");	// Purge all newline chars
+		AssetText = AssetText.Replace("[",ItemName);
+		AssetText = AssetText.Replace("]",OtherChar);
 		while (AssetText.Length > 0) {
-		//for (int i = 1; i <= 6; i++) {    //IGNORE THIS LINE IT'S FOR TEST PURPOSES
-			Index = -1;
 			string TestString = AssetText;
 			TestString = TestString.Substring (1, TestString.Length - 1);// Ignore the first symbol
-			int PIndex = TestString.IndexOf ("(");
-			int DIndex = TestString.IndexOf (")");	//Figure out whether a ( or ) is closest
-			if (PIndex < DIndex && PIndex > 0) {
-				Index = PIndex;
-			} else if (DIndex > 0) { 
-				Index = DIndex;		// Store its position
-			}
-			if (Index != -1) {		// If we found a ( or )
-				string Substring = AssetText.Substring (0, Index + 1);	// Get the substring from it
-				SpeechList.Add (Substring); 						// Add to the arraylist
-				string TempText = AssetText.Substring (Index + 1, AssetText.Length - Index - 1);
-				AssetText = TempText;					// Repeat with the rest of the string
-			} else {							// If there isn't one...
+			Debug.Log(TestString);
+			int LIndex = TestString.IndexOf ("(");
+			int RIndex = TestString.IndexOf (")");	//Figure out whether a (, ) or # is closest to the start
+			int HIndex = TestString.IndexOf("#");
+			if (LIndex == -1 && RIndex == -1 && HIndex == -1) {
 				string Substring = AssetText;
 				SpeechList.Add (Substring);		// The rest of the string gets added to the arraylist
+				Debug.Log(Substring);
 				AssetText = "";		// Then clear out the text
+			} else {
+				if (LIndex == -1) {LIndex = 9999999;}
+				if (RIndex == -1) {RIndex = 9999999;}
+				if (HIndex == -1) {HIndex = 9999999;}
+				int Index = Mathf.Min (LIndex, RIndex, HIndex);
+				string Substring = AssetText.Substring (0, Index + 1);	// Get the substring from it
+				SpeechList.Add (Substring); 						// Add to the arraylist
+				Debug.Log(Substring);
+				string TempText = AssetText.Substring (Index + 1, AssetText.Length - Index - 1);
+				AssetText = TempText;					// Repeat with the rest of the string
 			}
 		}
+		Array2Dict ();
+
+		SetBranch ("INTRO"); //TODO: DELETE THIS LINE IT IS FOR TEST PURPOSES ONLY
 	}
 
+	private void Array2Dict(){
+		ArrayList CurrentBranch = new ArrayList();
+		string BranchName = "";
+		for (int i = 0; i < SpeechList.Count; i++) {
+			string TestLine = SpeechList[i].ToString();
+			string TestChar = TestLine[0].ToString();
+			if (TestChar == "#" && CurrentBranch.Count != 0) {
+				SpeechDict.Add(BranchName, CurrentBranch);
+				CurrentBranch = new ArrayList();
+				BranchName = TestLine.Substring(1, TestLine.Length - 1);
+			} else if (TestChar == "#") {
+				BranchName = TestLine.Substring(1, TestLine.Length - 1);
+			} else {
+				CurrentBranch.Add(TestLine);
+			}
+		}
+		SpeechDict.Add(BranchName, CurrentBranch);
+	}
 
-	// tl;cbacommenting: Everytime you call Ping() it will return the next line of the arraylist
+	public void SetBranch (string BranchName) {
+		if (Pos != 0) {
+			Debug.LogWarning ("SetBranch called in middle of branch? Going through with it, resetting branch.");
+			Pos = 0;
+		}
+		BranchList = SpeechDict [BranchName];
+	}
+
+	// tl;dr: Everytime you call Ping() it will return the next line of the current branch
 	// When it's done you get a null string, which can be interpreted as required
 	public string Ping () {
-		if (0 <= Pos && Pos < SpeechList.Count) {
-			string ReturnString = SpeechList [Pos].ToString ();
+		if (0 <= Pos && Pos < BranchList.Count) {
+			string ReturnString = BranchList [Pos].ToString ();
 			Pos += 1;
 			return ReturnString;
-		} else if (Pos >= SpeechList.Count) {
+		} else if (Pos >= BranchList.Count) {
 			Pos = 0;
 			string NullString = null;
 			return NullString;
@@ -61,12 +96,13 @@ public class ImportSpeech : MonoBehaviour {
 		}
 	}
 
-	//This shit is gonna be rewritten into something relevent it's just here for demonstration
+	//This bit is gonna be rewritten into something relevent it's just here for demonstration
 	public void TestScript(){
 		string InString = Ping ();
 		if (InString == null) {
 			transform.position = NeutralPos;
 			GetComponentInChildren<Text>().text = "~~END OF MESSAGE~~";
+			SetBranch ("QUACK");
 		} else if (InString [0].ToString() == "(") {
 			transform.position = LeftPos;
 			string MemeString = InString.Substring (1, InString.Length - 1);
@@ -80,3 +116,5 @@ public class ImportSpeech : MonoBehaviour {
 		}
 	}
 }
+
+
