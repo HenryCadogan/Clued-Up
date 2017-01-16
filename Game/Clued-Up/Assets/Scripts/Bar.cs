1ff2a92b59690f4918d92675874dee3c1bd00ccf
Toby Fox <tobias@Tobiass-MacBook-Pro.local>
2017-01-16 15:37:58 +0000

    BEER POURING, Clue positions, but mainly BEER POURING

---
+++ Game/Clued-Up/Assets/Scripts/Bar.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class Bar : MonoBehaviour {
+
+	public Image fullPint;
+	public float speed;
+	private bool hasBeenPoured = false;
+	private bool pouring = false;
+
+	/// <summary>
+	/// When Pump is clicked begin pouring
+	/// </summary>
+	void OnMouseDown(){
+		if (!hasBeenPoured) {
+			hasBeenPoured = true;
+			pouring = true;
+		}
+	}
+	/// <summary>
+	/// Initialises full pint underneath pint mask 
+	/// </summary>
+	void Start(){
+		Vector3 pos = fullPint.transform.localPosition;
+		pos.y = -1200;
+		fullPint.transform.localPosition = pos;
+	}
+	/// <summary>
+	/// If beer is pouring, raise its posiion
+	/// </summary>
+	void Update(){
+		if (pouring) {
+			if (fullPint.transform.localPosition.y >= 0) {
+				pouring = false;
+			} else {
+				Vector3 pos = fullPint.transform.localPosition;
+				pos.y += speed * 10;
+				fullPint.transform.localPosition = pos;
+			}
+		}
+	}
+}
