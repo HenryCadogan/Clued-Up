0cb9eef35dfb48ed7f67b4cd49d3b23d5e8674c3
Henry <hgc509@york.ac.uk>
2017-01-18 23:02:16 +0000

    Tried another method of getting the map to work. Might do with some work but unsure if this is the best method of going about it

---
+++ Game/Clued-Up/Assets/AtriumMoveScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class AtriumMoveScript : MonoBehaviour {
+
+	GameObject mPanel;
+	// Use this for initialization
+	void Start () {
+		mPanel = GameObject.Find ("MapPanel");
+	}
+
+	void OnMouseDown(){
+		
+		print ("Collider pressed, moving to atrium");
+		//fade out, hide the menu and then load the scene
+		//mPanel.SetActive(false);
+		//Nasty hard coded value here but simply look in the build settings for 
+		//SceneManager.LoadScene(4);
+	}
+}
---
+++ Game/Clued-Up/Assets/MapController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapController : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+		GameObject Colliders = GameObject.Find("MapColliders");
+		Physics.queriesHitTriggers = true;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnMouseDown(){
+		Debug.Log ("Mouse pressed");
+	}
+	void printThing(){
+		Debug.Log ("Thing");
+	}
+
+}
