a0c870cc50e1506ab13ec0dce5702a722310f6f8
Toby Fox <tobias@Tobiass-MacBook-Pro.local>
2017-01-20 02:51:16 +0000

    Bar music

--- Game/Clued-Up/Assets/Scripts/Bar.cs
+++ Game/Clued-Up/Assets/Scripts/Bar.cs
@@ -6,15 +6,38 @@ public class Bar : MonoBehaviour {
 	public Image fullPint;
 	public Image stream;
 	public AudioSource source;
+	public AudioSource jukeboxSource;
 	public float speed;
+	public int song;
 	private bool hasBeenPoured = false;
 	private bool pouring = false;
+
 
 	/// <summary>
 	/// When Pump is clicked begin pouring
 	/// </summary>
 	void OnMouseDown(){
-		if (!hasBeenPoured) {
+		if (song != -1) {	//if there is a song property, play song
+			switch (song) {
+			case 0: 
+				jukeboxSource.clip = Resources.Load<AudioClip> ("Sounds/rock-song1");
+				break;
+			case 1: 
+				jukeboxSource.clip = Resources.Load<AudioClip> ("Sounds/rock-song2");
+				break;
+			case 2: 
+				jukeboxSource.clip = Resources.Load<AudioClip> ("Sounds/dubstep-song");
+				break;
+			case 3: 
+				jukeboxSource.clip = Resources.Load<AudioClip> ("Sounds/acoustic-song");
+				break;
+			default:
+				throw new System.ArgumentOutOfRangeException ("Song index is not valid");
+
+			}
+			jukeboxSource.Play ();
+
+		}else if (!hasBeenPoured) { //otherwise the beer must have been clicked.
 			source.Play ();
 			hasBeenPoured = true;
 			pouring = true;
@@ -24,9 +47,11 @@ public class Bar : MonoBehaviour {
 	/// Initialises full pint underneath pint mask 
 	/// </summary>
 	void Start(){
-		Vector3 pos = fullPint.transform.localPosition;
-		pos.y = -1200;
-		fullPint.transform.localPosition = pos;
+		if (song == -1) { //the tap
+			Vector3 pos = fullPint.transform.localPosition;
+			pos.y = -1200;
+			fullPint.transform.localPosition = pos;
+		}
 	}
 	/// <summary>
 	/// If beer is pouring, raise its posiion
@@ -47,4 +72,6 @@ public class Bar : MonoBehaviour {
 			}
 		}
 	}
+
+
 }
